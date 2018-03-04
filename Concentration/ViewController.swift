7aac02f8ee8f84be43163d755f54cc48eb8b87a7
Gagandeep Kalra <gagandeepkalra1994@gmail.com>
2018-03-04 17:19:49 +0530

    added gitignore file and Auto-Layout config

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -69,31 +69,5 @@ class ViewController: UIViewController {
         }
         return emoji[card.identifier] ?? "?"
     }
-    
-//    func flipCard(withEmoji emoji: String, on button: UIButton) {
-//        if button.currentTitle == emoji {
-//            resetCardButton(on: button)
-//        } else {
-//            button.setTitle(emoji, for: UIControlState.normal)
-//            UIView.animate(withDuration: 2, animations: {
-//                button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
-//            })
-//        }
-//    }
-//
-//    func resetCardButton(on button: UIButton) {
-//        UIView.animate(withDuration: 2, animations: {
-//            button.setTitle("", for: UIControlState.normal)
-//            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-//        })
-//        shuffleCards()
-//    }
-//
-//
-//    func setCardButton(on button: UIButton) {
-//
-//    }
-    
-    
 }
 
